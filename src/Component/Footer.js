0cd8747653a2e10380df1f9fd47feb8aefff4bf5
pranshi sahu <pranshi864@gmail.com>
2025-04-14 13:33:13 +0530

    footer

---
+++ src/Component/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='bg-black text-white w-0 h-7 p-9 m-5'>
+            Footer
+        </div>
+    )
+}
+
+export default Footer
