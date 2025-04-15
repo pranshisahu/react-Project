757e34ba53a98a0e47af60b7d54938f173bc6b1d
pranshi sahu <pranshi864@gmail.com>
2025-04-15 12:27:56 +0530

    update footer

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -2,10 +2,104 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='bg-black text-white w-0 h-7 p-9 m-5'>
-            Footer
-        </div>
-    )
+        <div className='bg-black text-white w-full h-full'>
+            <div className='bg-black text-white flex'>
+                <ul className=' w-full h-full p-9 flex-1'>
+                    <li><h2>Insurence</h2></li>
+                    <li><a href="genral">General insurence</a></li>
+                    <li><a href="life">life insurence</a></li>
+                    <li><a href="team">Team Insurence</a></li>
+                    <li><a href="invest">investMent</a></li>
+                    <li><a href="health">Health Insurence</a></li>
+                    <li><a href="other">Other Insurence</a></li>
+
+
+                </ul>
+                <ul className='flex-1'>
+                    <li><h2>Calculator</h2></li>
+                    <li><a href="SIP">SIP Calculator</a></li>
+                    <li><a href="Income">Income Tax Calculator</a></li>
+                    <li><a href="compound">Compound interest Calculator</a></li>
+                    <li><a href="nps">NPS Calculator</a></li>
+                    <li><a href="term">Term Insurence Calculator</a></li>
+                    <li><a href="hlv">HLV Calculator</a></li>
+                    <li><a href="life">Life Insurence Calculator</a></li>
+                    <li><a href="health">Health Insurence Calculator</a></li>
+                    <li><a href="travel">Travel Insurence Calculator</a></li>
+                    <li><a href="car">Car Insurence  Calculator</a></li>
+                    <li><a href="bike">Bike Insurence Calculator</a></li>
+                </ul>
+                <ul className='flex-1'>
+                    <li><h2>Resources</h2></li>
+                    <li><a href="Article">Articles</a></li>
+                    <li><a href="Customer">Customer review</a></li>
+                    <li><a href="insurence">Insurence Comppanies</a></li>
+                    <li><a href="news">Newsroom</a></li>
+                    <li><a href="award">Awards</a></li>
+                    <li><a href="pb">PB Life</a></li>
+                </ul>
+
+                <ul className='flex-1'>
+                    <li><h2>Company</h2></li>
+                    <li><a href="SIP">About Us</a></li>
+                    <li><a href="Income">Sitemap</a></li>
+                    <li><a href="compound">Careers</a></li>
+                    <li><a href="nps">Legal & Admin policies</a></li>
+                    <li><a href="term">ISNP</a></li>
+                    <li><a href="hlv">Contact  us</a></li>
+                    <li><a href="life">Verify Your Adviser</a></li>
+                    <li><a href="health">Investor Relation</a></li>
+                    <li><a href="travel">Travel Insurence Calculator</a></li>
+                    <li><a href="car">Car Insurence  Calculator</a></li>
+                    <li><a href="bike">Bike Insurence Calculator</a></li>
+                </ul>
+            </div>
+            <div className='flex flex-row ... bg-slate-600 justify-between scale-75'>
+                <div className='flex flex-row'>
+                    <h2>Payment Method</h2>
+                    <ul className='flex flex-row'>
+                        <li><img src="https://static.pbcdn.in/cdn/images/career/footer/amex-icon.png" /></li>
+                        <li><img src="https://static.pbcdn.in/cdn/images/career/footer/visa-icon.png" /></li>
+                        <li><img src="https://static.pbcdn.in/cdn/images/career/footer/paytm-icon.png" /></li>
+                        <li><img src="https://static.pbcdn.in/cdn/images/career/footer/rupay-icon.png" /></li>
+                        <li><img src="https://static.pbcdn.in/cdn/images/career/footer/netbanking-icon.png" /></li>
+                        <li><img src="https://static.pbcdn.in/cdn/images/career/footer/master-card-icon.png" /></li>
+
+                    </ul>
+                </div>
+                    <div>
+
+                        <ul className=' flex flex-row'>
+                            <h2></h2>
+                            <li><img src='https://static.pbcdn.in/cdn/images/career/footer/pci-icon.png' /></li>
+                        </ul>
+                    </div>
+                        <div>
+                            <ul className='flex flex-row text-sm'>
+                                <h2></h2>
+                                <li><img src='https://static.pbcdn.in/cdn/images/career/footer/facebook-icon.png?v=1' /></li>
+                                <li><img src='https://static.pbcdn.in/cdn/images/career/footer/twitter-icon.png?v=1' /></li>
+                                <li><img src='https://static.pbcdn.in/cdn/images/career/footer/linkedin-icon.png?v=1' /></li>
+                                <li><img src='https://static.pbcdn.in/cdn/images/career/footer/youtube-icon.png?v=1' /></li>
+
+                            </ul>
+                            </div>
+                        
+                        </div>
+                        <ul className='text-sm'>
+                            <li>Policybazaar Insurance Brokers Private Limited CIN: U74999HR2014PTC053454 Registered Office - Plot No.119, Sector - 44, Gurugram - 122001, Haryana Tel no. : 0124-4218302 Email ID: enquiry@policybazaar.com</li>
+                            <li>Policybazaar is registered as a Composite Broker | Registration No. 742, Registration Code No. IRDA/ DB 797/ 19, Valid till 09/06/2027, License category- Composite Broker</li>
+                            <li>Visitors are hereby informed that their information submitted on the website may be shared with insurers.Product information is authentic and solely based on the information received from the insurers</li>
+                            <li>BEWARE OF SPURIOUS PHONE CALLS AND FICTITIOUS / FRAUDULENT OFFERS</li>
+                            <li>IRDAI or its officials do not involve in activities like selling insurance policies, announcing bonus or investment of premiums. Public receiving such phone calls are requested to lodge a police complaint.</li>
+
+                        </ul>
+                        <ul>
+                            <li>© Copyright 2008-2025 policybazaar.com. All Rights Reserved.</li>
+                        </ul>
+                    </div>
+                    
+                    )
 }
 
-export default Footer
+                    export default Footer;
